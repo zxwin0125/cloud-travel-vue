5c870064becb5c52d8993e7a7f4e405eef426c16
zxwin <zxwin_0125@163.com>
2020-10-13 10:56:39 +0800

    :fire:目的地模块搭建

---
+++ travel/src/views/Destination/components/Destination_des.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="destination_des">
+    <!-- <Nav/> -->
+    <h2>这是目的地详情页</h2>
+  </div>
+</template>
+
+<script>
+import Nav from '../../../components/Nav'
+export default {
+  name: 'Destinatin_des',
+  components: {
+    Nav
+  }
+}
+</script>
