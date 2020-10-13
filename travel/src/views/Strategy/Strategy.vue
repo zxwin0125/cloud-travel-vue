31b400882def6dd9635b01502627ca628fb531ff
zxwin <zxwin_0125@163.com>
2020-10-13 11:02:23 +0800

    :fire:攻略模块搭建

---
+++ travel/src/views/Strategy/Strategy.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="strategy">
+    <Nav/>
+    <h2>这是攻略页面</h2>
+  </div>
+</template>
+
+<script>
+import Nav from '../../components/Nav'
+export default {
+  name: 'Strategy',
+  components: {
+    Nav
+  }
+}
+</script>
