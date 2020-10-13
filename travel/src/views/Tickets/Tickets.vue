eeb50d9f98265f884e19199c8f7a91a34866d350
zxwin <zxwin_0125@163.com>
2020-10-13 11:09:46 +0800

    :fire:购票商城模块搭建

---
+++ travel/src/views/Tickets/Tickets.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="tickets">
+    <Nav/>
+    <h2>这是票务页面</h2>
+  </div>
+</template>
+
+<script>
+import Nav from '../../components/Nav'
+export default {
+  name: 'Tickets',
+  components: {
+    Nav
+  }
+}
+</script>
