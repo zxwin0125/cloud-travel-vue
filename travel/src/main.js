e5fba9f5bf115f032d8f7283e8759e9254b6b23e
zxwin <zxwin_0125@163.com>
2020-10-13 11:27:28 +0800

    :fire:入口文件及路由配置

--- travel/src/main.js
+++ travel/src/main.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+
+// 引入 element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 引入 animate.css
+import animate from 'animate.css'
+
+// 使用 element-ui
+Vue.use(ElementUI)
+// 使用 animate.css
+Vue.use(animate)
 
 Vue.config.productionTip = false
 
+
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
--- travel/src/router/index.js
+++ travel/src/router/index.js
@@ -1,9 +1,23 @@
+// 路由相关配置
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+// 引入对应路由
+import Home from '../views/Home/Home.vue'
+import Destination from '../views/Destination/Destination.vue'
+import Destination_des from '../views/Destination/components/Destination_des.vue'
+import Strategy from '../views/Strategy/Strategy.vue'
+import Community from '../views/Community/Community.vue'
+import Tickets from '../views/Tickets/Tickets.vue'
+import DataView from '../views/DataView/DataView.vue'
+import Users from '../views/Person/Users.vue'
+import Login from '../views/Person/Login.vue'
+import Register from '../views/Person/Register.vue'
+
+// 使用路由
 Vue.use(VueRouter)
 
+// 定义路由
 const routes = [
   {
     path: '/',
@@ -11,17 +25,66 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/destination',
+    name: 'Destination',
+    component: Destination,
+    children: [
+      {
+        path: ':id',
+        component: Destination_des
+      }
+    ]
+  },
+  {
+    path: '/strategy',
+    name: 'Strategy',
+    component: Strategy
+  },
+  {
+    path: '/community',
+    name: 'Community',
+    component: Community
+  },
+  {
+    path: '/tickets',
+    name: 'Tickets',
+    component: Tickets
+  },
+  {
+    path: '/dataView',
+    name: 'DataView',
+    component: DataView
+  },
+  {
+    path: '/users',
+    name: 'Users',
+    component: Users
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    components: { 
+      default: Login,
+      'header':null
+    }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    components: { 
+      default: Register,
+      'header':null
+    }
+  },
+  {
+    path:'*',
+    redirect:'/home'
+  } 
 ]
 
+// 创建路由实例对象
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'history', // 路由的路径方式 history不加#
   base: process.env.BASE_URL,
   routes
 })
