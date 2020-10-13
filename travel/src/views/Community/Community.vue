07b336aa56822863cde657175ad1198e90580a68
zxwin <zxwin_0125@163.com>
2020-10-13 10:53:54 +0800

    :fire:社区模块搭建

---
+++ travel/src/views/Community/Community.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="community">
+    <Nav/>
+    <h2>这是社区页面</h2>
+  </div>
+</template>
+
+<script>
+import Nav from '../../components/Nav'
+export default {
+  name: 'Community',
+  components: {
+    Nav
+  }
+}
+</script>
