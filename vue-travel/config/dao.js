2730d8f2c08543dce44fde5c2124b5e67278ee91
zxwin0125 <zxwin_0125@163.com>
2021-04-11 23:33:19 +0800

    连接数据库

---
+++ vue-travel/config/dao.js
@@ -0,0 +1,47 @@
+// 连接数据库
+const mysql = require('mysql');
+
+function db(sql, params, cb) {
+    /**
+     * sql -> sql语句
+     * params -> 传参
+     * cb -> 回调函数
+     */
+
+    // 创建数据库连接
+    const connection = mysql.createConnection({
+        host: "localhost", // 主机名
+        port: 3306, // 端口号
+        database: "myserver", // 数据库
+        user: "root", // 用户名
+        password: "zx990125", // 密码
+        multipleStatements: true //支持多语句操作   是否允许在一个query中传递多个查询语句
+    })
+
+    // 连接数据库
+    connection.connect(
+        function (err) {
+            if (err) {
+                console.log("数据库连接失败");
+                res.end(
+                    JSON.stringify({
+                        code: 500,
+                        message: "无法查询数据"
+                    })
+                );
+            } else {
+                connection.query(sql, params, function(err, results) {
+                    if (err) {
+                        cb(err, null) // 执行回调函数，并且把参数返回
+                    } else {
+                        cb(null, results)
+                    }
+                })
+            }
+            connection.end()
+
+        }
+    )
+}
+
+modules.export = db
