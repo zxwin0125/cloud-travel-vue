0983c2049ceaf99af86195958caf91f4c14b38ca
zxwin0125 <zxwin_0125@163.com>
2021-04-17 12:49:19 +0800

    轮播图

--- vue-travel/client/src/views/Home/Home.vue
+++ vue-travel/client/src/views/Home/Home.vue
@@ -1,16 +1,30 @@
 <template>
   <section class="home">
-    
+    <!-- 轮播图开始 -->
+    <Swiper :imgs="imgs" :height="imgHeight" />
   </section>
 </template>
 
 <script>
-// 导入组件模块
-
-export default {
-  name: 'Home',
-  components: {
-    
+  // 导入组件模块
+  import Swiper from "../../components/Swiper"
+  export default {
+    name: 'Home',
+    components: {
+      Swiper
+    },
+    data() {
+      return {
+        // 图片地址数组
+        imgs: [
+          require('../../assets/img/Swiper/swiper01.png'),
+          require('../../assets/img/Swiper/swiper02.png'),
+          require('../../assets/img/Swiper/swiper03.png'),
+          require('../../assets/img/Swiper/swiper04.png'),
+          require('../../assets/img/Swiper/swiper05.png')
+        ],
+        imgHeight: '410px',
+      }
+    }
   }
-}
 </script>
