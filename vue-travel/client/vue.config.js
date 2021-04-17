0533c7c806e7c8aafdc4031a1f33bfef0a7e9ce8
zxwin0125 <zxwin_0125@163.com>
2021-04-17 17:00:51 +0800

    跨域配置

--- vue-travel/client/vue.config.js
+++ vue-travel/client/vue.config.js
@@ -1,10 +1,26 @@
 module.exports = {
+    // 路径配置
     configureWebpack: {
         resolve: {
             alias: {
                 'assets': '@/assets',
                 'components': '@/components',
                 'views': '@/views'
+            }
+        }
+    },
+    // 跨域
+    devServer: {
+        host: 'localhost',
+        port: '8080',
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
             }
         }
     }
