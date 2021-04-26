609f36a274162d140287cf2f0b850fa99cd200b3
zxwin0125 <zxwin_0125@163.com>
2021-04-26 20:55:36 +0800

    个人中心

---
+++ vue-travel/client/src/views/User/Users.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="users">
+    <Nav />
+    <!-- 头像 姓名 -->
+    <Usertop />
+    <Footer />
+  </div>
+</template>
+
+<script>
+  import Nav from '../../components/Nav'
+  import Footer from '../../components/Footer'
+  import Usertop from "../Person/components/Usertop";
+  import { getUser } from "@/api/getData.js"
+  export default {
+    name: 'Users',
+    components: {
+      Nav,
+      Footer,
+      Usertop
+    },
+    data() {
+      return {
+        // 个人信息
+        userInfo: []
+      }
+    },
+    // 定义方法
+    methods: {
+      
+    },
+    created() {
+      
+    },
+    mounted() {
+      //当页面渲染完成时调用方法获取数据
+      
+    }
+
+  }
+</script>
+<style lang="scss" scoped>
+</style>
