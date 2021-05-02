dc7f08f21c6ffb3341c96e77e33e85d97b492bd5
zxwin0125 <zxwin_0125@163.com>
2021-05-02 16:50:33 +0800

    getter/state

--- vue-travel/client/src/store/index.js
+++ vue-travel/client/src/store/index.js
@@ -3,13 +3,26 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const types = {
+  SET_IS_AUTNENTIATED: 'SET_IS_AUTNENTIATED', // 是否认证通过
+  SET_USER: 'SET_USER' // 用户信息
+}
+
+const state = { // 需要维护的状态
+  isAutnenticated: false,  // 是否认证
+  user_info: {}   // 存储用户信息
+}
+
+const getters = {
+  isAutnenticated: state => state.isAutnenticated,
+  user_info: state => state.user_info
+}
+
+
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+  mutations,
+  actions,
+  modules
 })
