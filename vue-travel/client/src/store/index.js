aba74e9e9f9282afe3a15e3a6487ef4aa7cb3044
zxwin0125 <zxwin_0125@163.com>
2021-05-02 16:53:13 +0800

    mutations

--- vue-travel/client/src/store/index.js
+++ vue-travel/client/src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 const types = {
   SET_IS_AUTNENTIATED: 'SET_IS_AUTNENTIATED', // 是否认证通过
-  SET_USER: 'SET_USER' // 用户信息
+  SET_USER_INFO: 'SET_USER_INFO' // 用户信息
 }
 
 const state = { // 需要维护的状态
@@ -15,7 +15,23 @@ const state = { // 需要维护的状态
 
 const getters = {
   isAutnenticated: state => state.isAutnenticated,
-  user_info: state => state.user_info
+  user_info: 
+  state => state.user_info
+}
+
+const mutations = {
+  [types.SET_IS_AUTNENTIATED](state, isAutnenticated) {
+    if (isAutnenticated)
+      state.isAutnenticated = isAutnenticated
+    else
+      state.isAutnenticated = false
+  },
+  [types.SET_USER_INFO](state, user_info) {
+    if (user_info)
+      state.user_info = user_info
+    else
+      state.user_info = {}
+  }
 }
 
 
