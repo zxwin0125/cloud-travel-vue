222a8cea6830ef2440955b1a6ecd503d08ae07be
zxwin0125 <zxwin_0125@163.com>
2021-05-03 13:55:43 +0800

    distination

---
+++ vue-travel/client/src/views/Destination/Destination.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="destination">
+    <!-- 导航栏 -->
+    <Nav />
+    <Destinationtop />
+    <!-- 热门目的地 -->
+    <Destinationveiw />
+    <!-- 图片风景分割线 -->
+    <Destinationdev />
+    <!-- 丽江风情 -->
+    <Destinationplace />
+    <Footer />
+  </div>
+</template>
+
+<script>
+  import Nav from '../../components/Nav';
+  import Destinationtop from '../Destination/components/Destinationtop';
+  import Destinationveiw from '../Destination/components/Destinationveiw';
+  import Destinationdev from '../Destination/components/Destinationdev';
+  import Destinationplace from '../Destination/components/Destinationplace';
+  import Footer from '../../components/Footer';
+  export default {
+    name: 'Destination',
+    components: {
+      Nav,
+      Footer,
+      Destinationtop,
+      Destinationveiw,
+      Destinationdev,
+      Destinationplace,
+    }
+  }
+</script>
+<style scoped>
+
+</style>
