85dad2192609271c25a45ae881d17688aa4069b9
zxwin0125 <zxwin_0125@163.com>
2021-05-03 15:17:43 +0800

    lj

---
+++ vue-travel/client/src/views/Destination/components/des_lj.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="des_lj">
+    <h2>目的地--丽江</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'des_lj',
+    components: {
+    }
+  }
+</script>
+<style scoped>
+
+</style>
