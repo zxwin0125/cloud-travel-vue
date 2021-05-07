a35814099f06dccf3ef44e68c699fcf918b90b59
zxwin0125 <zxwin_0125@163.com>
2021-05-08 07:59:36 +0800

    user样式

--- vue-travel/client/src/views/User/Users.vue
+++ vue-travel/client/src/views/User/Users.vue
@@ -20,6 +20,7 @@
         <i v-else class="el-icon-plus avatar-uploader-icon"></i>
       </el-upload>
       <div class="MAvaName">马蜂窝用户</div>
+      <!-- 图标 -->
       <div class="its_tags">
         <a href="/home/vip_show.php" target="_blank" title="VIP"
           ><i class="vip"></i
@@ -30,8 +31,9 @@
           ><i class="zhiluren"></i
         ></a>
       </div>
-      <div class="MAvaInfo clearfix MAvaMyInfo">
-        <span class="MAvaLevel flt1"
+      <!-- 等级 -->
+      <div class="MAvaInfo">
+        <span class="MAvaLevel"
           >等级：<a href="/rank/lv.php" title="Lv.1" target="_blank"
             >Lv.1</a
           ></span
@@ -39,6 +41,44 @@
         <span class="MAvaSet"
           ><a title="设置" href="/setting/" target="_blank"></a
         ></span>
+      </div>
+      <!-- 个人简介 -->
+      <div id="_j_profilearea" class="MAvaProfile">
+        <a role="button" class="addBtn _j_showintrobox">填写个人简介</a>
+        <div class="MAvaInput _j_inputarea hide">
+          <textarea
+            id="_j_introarea"
+            placeholder="例：摄影师/旅居澳洲/潜水爱好者"
+            maxlength="100"
+          ></textarea>
+          <a role="button" id="_j_introsaver" class="MAvaBtn" title="保存"
+            >保存</a
+          >
+        </div>
+      </div>
+      <div class="MAvaMore clearfix">
+        <div class="MAvaNums">
+          <strong
+            ><a href="/friend/index/follow?uid=68872750" target="_blank"
+              >7</a
+            ></strong
+          >
+          <p>关注</p>
+        </div>
+        <div class="MAvaNums">
+          <strong
+            ><a
+              href="/friend/index/follow?uid=68872750&amp;flag=1"
+              target="_blank"
+              >0</a
+            ></strong
+          >
+          <p>粉丝</p>
+        </div>
+        <div class="MAvaNums last">
+          <strong><a href="/mall/" target="_blank">1</a></strong>
+          <p>蜂蜜</p>
+        </div>
       </div>
     </div>
   </div>
@@ -159,49 +199,122 @@ export default {
         background-position: -72px -460px;
       }
     }
-    .side_bar .MAvaInfo {
-      padding: 0 15px;
+    .MAvaInfo {
+      padding: 0 60px;
+      display: flex;
+      align-items: center;
+      span {
+        padding: 0 15px 0 12px;
+        display: inline-block;
+        position: relative;
+      }
+      .MAvaLevel a {
+        color: #ea3c1a;
+        font-size: 16px;
+        font-weight: bold;
+        font-family: Arial;
+        &:after {
+          content: "";
+          background-color: #d6d6d6;
+          position: absolute;
+          top: 3px;
+          right: 0;
+          width: 1px;
+          height: 16px;
+        }
+      }
+      .MAvaSet {
+        padding: 0 0 0 20px;
+        position: relative;
+        top: -1px;
+        display: inline-block;
+        a {
+          width: 17px;
+          height: 17px;
+          background-image: url(../../assets/img/icon/ico_sprite_v8.png);
+          background-position: -108px -146px;
+          display: inline-block;
+          vertical-align: middle;
+        }
+      }
     }
-    .MAvaMyInfo span {
-      padding: 0 10px 0 7px;
+    .MAvaProfile {
+      margin-top: 20px;
+      padding: 0 20px;
+      .addBtn {
+        line-height: 30px;
+        border-radius: 3px;
+        color: #666;
+        background-color: #efefef;
+        display: block;
+        text-align: center;
+        transition: background-color 0.3s ease-out 0s;
+        -webkit-transition: background-color 0.3s ease-out 0s;
+      }
+      .hide {
+        display: none;
+      }
+      textarea {
+        height: 78px;
+        padding: 15px;
+        background-color: #efefef;
+        line-height: 20px;
+        color: #999;
+        border-style: none;
+        display: block;
+        width: 208px;
+        border: #efefef 1px solid;
+        font-size: 14px;
+        resize: none;
+        transition: 0.3s ease-out 0s;
+        -webkit-transition: 0.3s ease-out 0s;
+      }
+      .MAvaBtn {
+        line-height: 30px;
+        border-radius: 3px;
+        color: #fff;
+        background-color: #ccc;
+        margin-top: 10px;
+        display: block;
+        text-align: center;
+        transition: background-color 0.3s ease-out 0s;
+        -webkit-transition: background-color 0.3s ease-out 0s;
+        cursor: pointer;
+      }
     }
-    .MAvaInfo span {
-      padding: 0 15px 0 12px;
-      display: inline-block;
-      position: relative;
-    }
-    .flt1 {
-      float: left;
-    }
-    .MAvaInfo .MAvaLevel a {
-      color: #ea3c1a;
-      font-size: 16px;
-      font-weight: bold;
-      font-family: Arial;
-    }
-    .MAvaInfo .MAvaLevel:after {
-      content: "";
-      background-color: #d6d6d6;
-      position: absolute;
-      top: 0;
-      right: 0;
-      width: 1px;
-      height: 16px;
-    }
-    .MAvaMyInfo .MAvaSet {
-      padding: 0 0 0 5px;
-      position: relative;
-      top: -1px;
-      display: inline-block;
-    }
-    .MAvaMyInfo .MAvaSet a {
-      width: 17px;
-      height: 17px;
-      background-image: url(../../assets/img/icon/ico_sprite_v8.png);
-      background-position: -108px -146px;
-      display: inline-block;
-      vertical-align: middle;
-    }
+    .MAvaMore {
+    margin-top: 20px;
+    border-top: #e2e2e2 1px solid;
+    border-bottom: #e2e2e2 1px solid;
+}
+.MAvaMore .MAvaNums {
+    position: relative;
+    width: 33%;
+    float: left;
+    padding: 10px 0;
+    text-align: center;
+    line-height: 20px;
+}
+.MAvaMore .MAvaNums strong {
+    font-size: 20px;
+    color: #444;
+    font-family: Arial;
+}
+.MAvaMore .MAvaNums p {
+    font-size: 14px;
+}
+.MAvaMore .MAvaNums:after {
+    content: '';
+    width: 1px;
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: #e2e2e2;
+}
+.MAvaMore .MAvaNums strong a {
+    color: #444;
+}
   }
 }
 </style>
